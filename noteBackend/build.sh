f9b15883d693e79b22287ea21587adc329f929c0
maity <maity.in@gmail.com>
2025-12-07 13:03:33 +0530

    Change the build

--- noteBackend/build.sh
+++ noteBackend/build.sh
@@ -2,9 +2,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python ./noteBackend/manage.py collectstatic --no-input
 
-python manage.py migrate
+python ./noteBackend/manage.py migrate
 
 
 # if [[ $CREATE_SUPERUSER ]]
