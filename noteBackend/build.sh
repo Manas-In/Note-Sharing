dec44050fb3f87cd96081384d2440d9422fffe53
maity <maity.in@gmail.com>
2025-12-07 16:48:02 +0530

    Fixing setting

--- noteBackend/build.sh
+++ noteBackend/build.sh
@@ -1,5 +1,7 @@
+
+
 set -o errexit
 
 pip install -r backend/requirements.txt
-python ./backend/manage.py collectstatic --no-input
-python ./backend/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
